2fdbf7b774b217a0be078b38b6f8714c305b52da
DESKTOP-ELOHNJ3\Sarai <sarairestrepo22@gmail.com>
2021-12-12 23:33:56 -0500

    firts commit

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+"use strict"
+
+import express from 'express';
+const app = express();
+
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import authRoute from './routes/routes'
+
+// Configuracion
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', authRoute);
+
+// Conexión a la base de datos (cluster de Mongo)
+mongoose.connect(
+    'mongodb+srv://sarai:12345@cluster1.jr4tq.mongodb.net/pogar?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    },
+    () => console.log('Estas conectado a la base de datos')
+);
+
+app.listen(5001, () => console.log('Servidor corriendo en el puerto 5001'));
