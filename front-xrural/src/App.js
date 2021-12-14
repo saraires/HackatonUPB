26391ea4a0fce1e919d791bf73d59c8afae14212
jmchaparro <juanmanuel@jmchaparro.com>
2021-12-14 10:41:19 -0500

    base front

---
+++ front-xrural/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import UsersPage from "./pages/UsersPage";
+import UserPage from "./pages/UserPage";
+
+import Navbar from "./components/Navbar";
+import DashboardPage from "./pages/DashboardPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/users/*" element={<UserPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/myusers/" element={<Navigate replace to="/users" />} />
+        <Route path="/users/:id/" element={<UserPage />} />
+
+        <Route path="/dashboard/*" element={<DashboardPage />}>
+          <Route path="welcome" element={<p>Welcome!</p>} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+}
