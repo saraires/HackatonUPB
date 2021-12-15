ee83691d0f2b7cc0ac9634c906fd80e555c4d775
jmchaparro <juanmanuel@jmchaparro.com>
2021-12-15 16:09:51 -0500

    llamado axios listado en experiencias

--- front-xrural/src/pages/ExperienciasPage.js
+++ front-xrural/src/pages/ExperienciasPage.js
@@ -1,6 +1,15 @@
 import React, { Fragment } from "react";
+import axios from "../axios/axios";
+import { useState, useEffect } from "react";
 
 export default function ExperienciasPage() {
+  const [experiencias, setExperiencias] = useState([]);
+
+  useEffect(() => {
+    axios.get("/categoria/experiencia").then((res) => {
+      setExperiencias(res.data);
+    });
+  }, []);
   return (
     <Fragment>
       <div className="container">
@@ -11,7 +20,21 @@ export default function ExperienciasPage() {
       </div>
 
       <div className="container">
-        <div className="col text-center shadow-none p-3 mb-5 bg-light border rounded-3">Listado de Experiencias</div>
+        <div className="col text-center shadow-none p-3 mb-5 bg-light border rounded-3">
+          <tbody className="text-center table-bordered">
+            {experiencias.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td width="10%">{item.id}</td>
+                  <td width="20%">{item.nombre}</td>
+                  <td width="20%">{item.descripcion}</td>
+                  <td width="20%">{item.fecha_inicio}</td>
+                  <td width="20%">{item.fecha_fin}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </div>
       </div>
     </Fragment>
   );
