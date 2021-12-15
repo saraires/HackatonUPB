5bb25dc9b1645e4ee0f64be6564c0ec3049bf45b
DeLaHoz73 <DeLaHoz73.martinsierra1973@gamil.com>
2021-12-15 16:35:35 -0500

    Cambios hospedajes

--- front-xrural/src/pages/HospedajesPage.js
+++ front-xrural/src/pages/HospedajesPage.js
@@ -2,47 +2,66 @@ import React, { Fragment, useState, useEffect } from "react";
 import Axios from "../axios/axios";
 
 export default function HospedajesPage() {
-  const arrpreferencias = [];
+  
+
+  var arrprovi = [
+    {id:1122,atributos: ["wifi", "lavadora"]},
+    {id:1133,atributos: ["cocina", "lavadora", "aire"]},
+    {id:1144,atributos: ["aire", "lavadora"]},
+    {id:1155,atributos: ["parqueadero", "aire"]}
+  ]
+
+  var arrpreferencias=[] 
+  var arrfinal = arrprovi
 
   function preferencias(event) {
-    const valor = event.target.value;
-    if (event.target.checked) {
-      arrpreferencias.push(valor);
-    } else {
-      let identificador = arrpreferencias.indexOf(valor);
-      arrpreferencias.splice(identificador, 1);
+    const valor = event.target.value
+    if (event.target.checked){
+      arrpreferencias.push(valor)
     }
-    console.log(arrpreferencias);
+    else{
+      let identificador = arrpreferencias.indexOf(valor)
+      arrpreferencias.splice(identificador, 1)
+    }
+    arrfinal = []
+    console.log(arrpreferencias)
+    for (var k=0;k<arrpreferencias.length;k++){
+      var prefesolas=arrpreferencias[k]
+      console.log(prefesolas)
+      for (var i=0;i<arrprovi.length;i++){
+        var atribs = arrprovi[i].atributos
+        for(var j=0;j<atribs.length;j++){
+          var unoauno = atribs[j]
+          if (unoauno===prefesolas){
+            var idid = arrprovi[i].id
+            var siono = arrfinal.find( datoid => datoid.id === idid )
+            console.log(siono)
+            
+            arrfinal.push(arrprovi[i])
+          }
+        }
+      }
+    }
+    console.log(arrfinal)
   }
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    Axios.get({
-      url: "/servicios",
-    })
-      .then((response) => {
-        setList(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [setList]);
+  var prueba = 678678
+  
   return (
     <Fragment>
       <div className="container">
         <h1>Hospedajes</h1>
         <div className="row">
           <div className="col border rounded-3 m-3 text-center shadow-none p-3 mb-5">
-            <label>Wifi</label>
-            <input type="checkbox" value="1" name="o1" id="a" onChange={preferencias} />
+          <label>Wifi</label>
+            <input type="checkbox" value="wifi" name="o1" id="a" onChange={preferencias}/> 
             <label>Cocina</label>
-            <input type="checkbox" value="2" name="o2" id="b" onChange={preferencias} />
+            <input type="checkbox" value="cocina" name="o2" id="b" onChange={preferencias}/>
             <label>Aire Acondicionado</label>
-            <input type="checkbox" value="3" name="o3" id="c" onChange={preferencias} />
+            <input type="checkbox" value="aire" name="o3" id="c" onChange={preferencias}/>
             <label>Parqueadero</label>
-            <input type="checkbox" value="4" name="o4" id="d" onChange={preferencias} />
+            <input type="checkbox" value="parqueadero" name="o4" id="d" onChange={preferencias}/>
             <label>Lavadora</label>
-            <input type="checkbox" value="5" name="o5" id="e" onChange={preferencias} />
+            <input type="checkbox" value="lavadora" name="o5" id="e" onChange={preferencias}/>
           </div>
         </div>
       </div>
