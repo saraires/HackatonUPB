1fc72eb55b039daecae5bb66ec26f40de149d106
DeLaHoz73 <martinsierra1973@gmail.com>
2021-12-14 23:09:02 -0500

    Cambio ruta axios

--- front-xrural/src/axios/axios.js
+++ front-xrural/src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export default axios.create({baseURL: `http://localhost:5001`});
+export default axios.create({baseURL: `http://167.99.119.181:5051`});
 
 // import axios from '../axios/axios';
 // axios.get('/ruta del back tal cual', {
