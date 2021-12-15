460ec1cb08bcecf0050b355e65d5037e7304b59f
Karen <quemojojojo@hotmail.com>
2021-12-15 17:44:24 -0500

    FormPlanes listo

--- front-xrural/src/components/FormPlanes.jsx
+++ front-xrural/src/components/FormPlanes.jsx
@@ -66,7 +66,6 @@ export default function FormPlanes(categoria,tags,atributos){
 
                 <label className="float-start mt-4" htmlFor="video">Si quieres compartir un video de youtube, pega el link!</label>
                 <input nombre="video" className="form-control" type="text"/>
-                {errors.video && <div className="alert alert-danger" role="alert">A description is required</div>}                
                 
                 <label className="float-start mt-4" htmlFor="descripcion">Cuéntanos lo que incluye el plan</label>
                 <textarea name="descripcion" className="form-control" type="text" cols="40" rows="5" {...register('descripcion',{required:true,maxLegth:1000})}/>
@@ -81,20 +80,20 @@ export default function FormPlanes(categoria,tags,atributos){
                 {errors.municipio && <div className="alert alert-danger" role="alert">El campo es requerido</div>}
 
                 <label className="float-start mt-4" htmlFor="direccion">Si el lugar que ofreces tiene dirección, escribela aquí</label>
-                <input name="direccion" className="form-control" type="text" {...register('direccion',{required:true,maxLegth:10})}/>
+                <input name="direccion" className="form-control" type="text" {...register('direccion',{required:true,maxLegth:100})}/>
                 {errors.direccion && <div className="alert alert-danger" role="alert">El campo es requerido</div>}
 
                 <label className="float-start mt-4" htmlFor="detalleUbicacion">Si el lugar que ofreces no cuenta con dirección, dale a tus visitantes indicaciones sobre cómo llegar</label>
-                <input name="detalleUbicacion" className="form-control" type="text" {...register('detalleUbicacion',{required:true,maxLegth:10})}/>
+                <input name="detalleUbicacion" className="form-control" type="text" {...register('detalleUbicacion',{required:true,maxLegth:100})}/>
                 {errors.detalleUbicacion && <div className="alert alert-danger" role="alert">El campo es requerido</div>}
 
                 <label className="float-start mt-4" htmlFor="capacidad">¿Para cuántas personas tiene capacidad?</label>
-                <input name="capacidad" className="form-control" type="number" {...register('capacidad',{required:true,maxLegth:10})}/>
-                {errors.capacidad && <div className="alert alert-danger" role="alert">Price is required</div>}
+                <input name="capacidad" className="form-control" type="number" {...register('capacidad',{required:true,maxLegth:20})}/>
+                {errors.capacidad && <div className="alert alert-danger" role="alert">El campo es requerido</div>}
 
                 <label className="float-start mt-4" htmlFor="precio">¿Cuál es el precio del plan?</label>
-                <input name="precio" className="form-control" type="number" {...register('precio',{required:true,maxLegth:10})}/>
-                {errors.precio && <div className="alert alert-danger" role="alert">Price is required</div>}
+                <input name="precio" className="form-control" type="number" {...register('precio',{required:true,maxLegth:20})}/>
+                {errors.precio && <div className="alert alert-danger" role="alert">El campo es requerido</div>}
 
                 {/* <button className='btn btn-success btn-sm' onClick={this.enviarDatos} type="button" >Save!</button> */}
                 <input className='btn btn-success btn-sm' type= 'submit' value='Save!'/>
